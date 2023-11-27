2f5726671f588e764d26244612ac5f05bc9f49ec
liliird <142356931+liliird@users.noreply.github.com>
2023-11-26 22:45:17 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-// main.cpp
-
-#include "Estudiante.h"
+#include <iostream>
+#include "Persona.h"
 #include "Profesor.h"
+#include "Estudiante.h"
+
+#include <vector>
+#include <typeinfo>
+
+
+using namespace std;
 
 int main() {
-  // Crear instancias de Estudiante y Profesor
-  Estudiante estudiante("Juan", 20, "Matemáticas");
-  Profesor profesor("Diana", 35, "Física");
 
-  // Invocar métodos
-  estudiante.presentarse();
-  estudiante.estudiar();
 
-  profesor.presentarse();
-  profesor.ensenar();
+    vector<Persona*> personas;
+    Profesor* prof = new Profesor("Raúl Pérez",45, "Matemáticas");
+    personas.push_back(prof);
 
-  return 0;
+  Estudiante* est = new Estudiante("Paola Vera",18, "Humanidades");
+  personas.push_back(est);
+  
+    cout << "------------ PROFESOR ------------" << endl;
+    cout << prof->toString();
+
+    cout << "------------  ALUMNO ------------" << endl;
+    cout << est->toString();
+
+    return 0;
 }
